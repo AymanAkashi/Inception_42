e9d26c642c9954c06e8d19d3af987f6928cb7a77
AymanAkashi <aaggoujj@student.1337.ma>
2023-02-26 16:50:43 -0500

    feat : add configuration of php in config file

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-apk update && apk upgrade && apk add --no-cache nginx netcat-openbsd bc curl wget git bash openssl libressl php7 php7-fpm php7-opcache
+apk add --no-cache nginx openssl
 mkdir -p /etc/nginx/ssl
-openssl req -x509 -new -newkey rsa:4096 -nodes -keyout /etc/nginx/ssl/aaggoujj.42.fr.key -out /etc/nginx/ssl/aaggoujj.42.fr.crt -subj "/C=MA/ST=/L=/O=/OU=/CN=aaggoujj.42.fr"
+openssl req -x509 -new -newkey rsa:4096 -nodes -keyout /etc/nginx/ssl/aaggoujj.key -out /etc/nginx/ssl/aaggoujj.crt -subj "/C=MA/ST=/L=/O=/OU=/CN=aaggoujj.42.fr"
