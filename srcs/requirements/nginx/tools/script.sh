2bfc894c24c7639834bfdbbe6e2c490b201188bc
AymanAkashi <aaggoujj@student.1337.ma>
2023-03-03 16:55:20 -0500

    style : add Header for some file

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+#   _   _   _     _   _   _   _   _   _   _   _   _   _   _  
+#  / \ / \ / \   / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ 
+# ( S | S | L ) ( C | e | r | t | i | f | i | c | a | t | e )
+#  \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ 
+
+# *************************************************************************** #
 apk add --no-cache nginx openssl
 mkdir -p /etc/nginx/ssl
 openssl req -x509 -new -newkey rsa:4096 -nodes -keyout /etc/nginx/ssl/aaggoujj.key -out /etc/nginx/ssl/aaggoujj.crt -subj "/C=MA/ST=/L=/O=/OU=/CN=aaggoujj.42.fr"
+# *************************************************************************** #
